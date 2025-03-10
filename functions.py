e55914afdadb731efa60360e437e7924930db07e
Lorbru <135026945+Lorbru@users.noreply.github.com>
2025-03-10 23:04:28 +0100

    commit

---
+++ functions.py
@@ -0,0 +1,12 @@
+# import 
+import numpy as np
+import pandas as pd
+from torch.utils.data import Dataset
+import torch
+
+class LoadDataset(Dataset):
+
+    def __init__(self):
+        return
+    
+    
